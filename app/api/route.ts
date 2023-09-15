ace7e8c4d0a1336610d688f1759d875b3546920c
Yevhen Humen <105159428+ehumen@users.noreply.github.com>
2023-09-15 19:52:19 +0300

    Delete app/api directory

--- app/api/route.ts
+++
@@ -1,9 +0,0 @@
-export async function handler(setState) {
-    let response = await fetch('/data.json')
-        .then(response => response.json())
-        .then(json => {
-                setState(json)
-            }
-        );
-
-}
